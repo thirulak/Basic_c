19472e46d43735f726022b62588d2eaeced3bb4d
thiru <36688218+thirulak@users.noreply.github.com>
2019-04-14 22:47:41 +0530

    Update 2017set2.c

--- 2017set2.c
+++ 2017set2.c
@@ -10,8 +10,8 @@ int main()
 void printxy (int x, int y)   {
       int *ptr; //pointer is created which contain integer value.
       x = 0; //value of x = 0 here.
-      ptr = &x; // ptr point to variable which has 0 
+      ptr = &x; // ptr points to variable which has 0 
       y = *ptr; // y contain value pointed by ptr i.e. x= 0;
-      *ptr = 1; // value pointer by ptr is now set to 1 i.e. x= 1;
-      printf("%d%d",x,y); // print x,y = x= 1 y=0
+      *ptr = 1; // value pointed by ptr is now set to 1 i.e. x= 1;
+      printf("%d%d",x,y); // print x,y i.e:x= 1 y=0
 }
