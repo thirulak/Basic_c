5c5f5a355cd13bd8d7751c92c30208f7c8dacfab
thiru <36688218+thirulak@users.noreply.github.com>
2019-04-14 22:43:02 +0530

    Create 2017set2.c

---
+++ 2017set2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void printxy(int x, int y);
+
+int main()
+{
+  int x,y;
+  printxy(x,y);
+  return 0;
+}
+void printxy (int x, int y)   {
+      int *ptr; //pointer is created which contain integer value.
+      x = 0; //value of x = 0 here.
+      ptr = &x;
+      y = *ptr; // y contain value pointed by ptr i.e. x= 0;
+      *ptr = 1; // value pointer by ptr is now set to 1 i.e. x= 1;
+      printf("%d%d",x,y);
+}
