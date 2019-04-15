ffce274a6efb0bb81ccb01f10530064129bf7f59
thiru <36688218+thirulak@users.noreply.github.com>
2019-04-15 18:07:30 +0530

    Create typecastuse.c

---
+++ typecastuse.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<string.h>
+int main ()  {
+  char* c = "GATECSIT2017";
+  char* p = c;
+  printf("%d", (int) strlen (c + 2[p] - 6[p] - 1));
+  return 0;
+}
+//Here typecasting is done , and so the value of alphabets are changed to their ASCII values 
