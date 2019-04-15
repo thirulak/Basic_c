4f12704b860063c3adcc49a1ff54e2e0dd4ee6d1
thiru <36688218+thirulak@users.noreply.github.com>
2019-04-15 12:50:22 +0530

    Update 2017set2.c

--- 2017set2.c
+++ 2017set2.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+[1]#include <stdio.h>
 void printxy(int x, int y);
 
 int main()
@@ -6,7 +6,7 @@ int main()
   int x,y;
   printxy(x,y);
   return 0;
-}
+}arra
 void printxy (int x, int y)   {
       int *ptr; //pointer is created which contain integer value.
       x = 0; //value of x = 0 here.
@@ -15,3 +15,28 @@ void printxy (int x, int y)   {
       *ptr = 1; // value pointed by ptr is now set to 1 i.e. x= 1;
       printf("%d%d",x,y); // print x,y i.e:x= 1 y=0
 }
+
+[2]int main ()  {
+      int array[] = {3, 5, 1, 4, 6, 2};
+      int done = 0;
+      int i;
+
+      while (done == 0)  {
+           done = 1;
+           for (i=0; i<=4; i++)  {
+                if (array[i] < array[i+1])  {
+                       swap (&array[i], &array[i+1]);
+                       done = 0;
+                }
+           }
+           for (i=5; i>=1; i--)  {
+                if (array[i] > array[i-1])   {
+                      swap(&array[i], &array[i-1]);
+                      done=0;
+                }
+           }
+       }
+       printf("%d", array[3]);
+}
+//the while loop terminates after making done=1 it happens after arranging all the array values in descending order
+//and so it prints the ouptput as array[3]
